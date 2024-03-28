63b9157b9974e51dbb662a6d2e7f5fd8c051ab00
Iurii Pavliuk <iurii.pavliuk@unity3d.com>
2024-03-28 11:31:16 +0100

    set target framerate trigger

--- Assets/Editor/SetTargetFramerateTrigger.cs
+++ Assets/Editor/SetTargetFramerateTrigger.cs
@@ -1,13 +1,27 @@
 using UnityEditor;
 using UnityEngine;
-namespace Editor {
-public class SetTargetFramerateTrigger : IRemoteDebugPanelWindowItem  {
-private string _input = "";
-	[InitializeOnLoadMethod] private static void OnDomainReload() { RemoteDebugPanelWindow.Items.Add(new SetTargetFramerateTrigger()); }
-	public void OnRender() {
-		GUILayout.Label("Set target framerate");
-	_input = GUILayout.TextField(_input);
-	if (GUILayout.Button("Set target framerate")) {Debug.Log("Hello from RemoteDebugPanelCallbacks.SetTargetFramerate ");RemoteDebugPanelCallbacks.SetTargetFramerate(int.Parse(_input));}
-	}
-}
+
+namespace Editor
+{
+    public class SetTargetFramerateTrigger : IRemoteDebugPanelWindowItem
+    {
+        private string _input = "";
+
+        [InitializeOnLoadMethod]
+        private static void OnDomainReload()
+        {
+            RemoteDebugPanelWindow.Items.Add(new SetTargetFramerateTrigger());
+        }
+
+        public void OnRender()
+        {
+            GUILayout.Label("Set target framerate");
+            _input = GUILayout.TextField(_input);
+            if (GUILayout.Button("Set target framerate"))
+            {
+                Debug.Log("Hello from RemoteDebugPanelCallbacks.SetTargetFramerate ");
+                RemoteDebugPanelCallbacks.SetTargetFramerate(int.Parse(_input));
+            }
+        }
+    }
 }
